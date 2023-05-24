cf62275d9c5b49e1a68f06c406ebaa309dbcf608
willy <willy.maykros.romero@gmail.com>
2023-05-24 14:51:29 -0500

    test_too_many_problems rocks

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,10 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, results=True):
+
+    if len(problems) > 5:
+        return 'Error: Too many problems.'
+    
+    if results:
+        print(problems)
 
 
-    return arranged_problems
+    # return arranged_problems
