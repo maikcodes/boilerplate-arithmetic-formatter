dbb8e1a9f56afc65c46aaeba962c875d485fd319
willy <willy.maykros.romero@gmail.com>
2023-05-24 15:32:09 -0500

    test_incorrect_operator and test_only_digits rocks

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,10 +1,45 @@
+import re
+
+
+def check_operation(problem):
+    pattern = r'[-+]'
+    if not re.search(pattern, problem):       
+        return (False, problem)
+    
+    return (True, problem.split())
+
+
+def check_operations(problems):
+    array_problems = []
+    for problem in problems:
+        valid_operation, array_problem = check_operation(problem)
+        if not valid_operation:
+            return False
+        
+        array_problems.append(array_problem)
+
+    return array_problems
+    
+def check_digits(array_problems):
+    for problem in array_problems:
+        if (type(problem[0]) is str) and (type(problem[2]) is str):
+            return False
+    return True
+
 def arithmetic_arranger(problems, results=True):
 
     if len(problems) > 5:
         return 'Error: Too many problems.'
     
     if results:
-        print(problems)
+        array_problems = check_operations(problems)
+        if not array_problems:
+            return "Error: Operator must be '+' or '-'."
+        
+        if not check_digits(array_problems):
+            return 'Error: Numbers must only contain digits.'
+        
+        print(array_problems)        
 
 
     # return arranged_problems
